99678e62d71fe9dcf3eb3ba754ee6ff095784368
JDumadag25 <justin.dumadag@gmail.com>
2018-08-15 22:47:42 -0400

    beginning delete fave function

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Favorites = (props) => {
   return(
-    <h4><a href={props.favorite.link} target="_blank">{props.favorite.name}</a></h4>
+    <div>
+      <h4><a href={props.favorite.link} target="_blank">{props.favorite.name}</a></h4>
+      <button onClick={this.handleDelete}>Delete</button>
+    </div>
     )
 }
 
